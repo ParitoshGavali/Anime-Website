02bada868d50404efd8d596f99d95d34b9130b69
Rupesh282 <rupu.kalantre@gmail.com>
2020-08-05 22:57:30 +0530

    added animedia project

--- Animedia/animelist/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Anime , Tag
-
-# Register your models here.
-admin.site.register(Anime)
-admin.site.register(Tag)
--- Animedia/animelist/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AnimelistConfig(AppConfig):
-    name = 'animelist'
