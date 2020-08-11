10023e7fc84599835651b423ddbc6d564653e4d5
Amrut <rxyamrutu@gmail.com>
2020-08-11 21:33:23 +0530

    created home app(empty template)

--- Animedia/Animedia/urls.py
+++ Animedia/Animedia/urls.py
@@ -27,6 +27,7 @@ urlpatterns = [
     path('register/' , users_views.register , name = 'register'),
     path('login/' , auth_views.LoginView.as_view(template_name='users/login.html') , name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html')  , name='logout'),
+    path('', include('home.urls'), name='home')
 ]  
 
 
